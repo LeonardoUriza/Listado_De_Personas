73f9cc897eca5689d6b8e97c281136a91f2f9535
LeonardoUriza <ur.leo16@gmail.com>
2023-04-27 18:38:24 -0500

    Listado-Personas

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const personas = [
+    new Persona("Juan", "Pérez"),
+    new Persona("Karla", "Zambrano")
+];
+
+function mostrarPersonas() {
+    console.log("Mostrar Personas...");
+    let texto = "";
+    for (let persona of personas) {
+        console.log(persona);
+        texto += `<li> ${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona() {
+    const forma = document.forms["forma"];
+    const nombre = forma["nombre"];
+    const apellido = forma["apellido"];
+    if (nombre.value != "" && apellido.value != "") {
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    } else{
+        alert("No hay Información que agregar...");
+        console.log("No hay información que agregar...");
+    }
+
+}
